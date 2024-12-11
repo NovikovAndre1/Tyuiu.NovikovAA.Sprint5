0b29793f6d58266cb2955e6db15f556b234ee5aa
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-12-11 18:35:29 +0600

    Task1

--- Tyuiu.NovikovAA.Sprint5.Task1.V10.Lib/DataService.cs
+++ Tyuiu.NovikovAA.Sprint5.Task1.V10.Lib/DataService.cs
@@ -15,22 +15,18 @@ namespace Tyuiu.NovikovAA.Sprint5.Task1.V10.Lib
 
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
-
             
-
             if (fileExists)
             {
                 File.Delete(path);
             }
-
-
 
             double y;
             string strY;
             for (int x = startValue; x <= stopValue; x++)
             {
                 y = (2 * Math.Cos(x) + 2) / (2 * x - 1) + Math.Cos(x) - 5 * x + 3;
-                Math.Round(y, 2);
+                string formattedY = string.Format("{0:F2}", Math.Round(y, 2));
                 strY = Convert.ToString(y);
 
                 if (x != stopValue)
