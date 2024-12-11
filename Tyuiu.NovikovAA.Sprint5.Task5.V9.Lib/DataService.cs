a55f7653ca8328e7143ac1c116e6a52396fddb97
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-12-12 03:40:20 +0600

    Task5

--- Tyuiu.NovikovAA.Sprint5.Task5.V9.Lib/DataService.cs
+++ Tyuiu.NovikovAA.Sprint5.Task5.V9.Lib/DataService.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Globalization;
+using System.IO;
 using tyuiu.cources.programming.interfaces.Sprint5;
 
 
@@ -8,39 +9,34 @@ namespace Tyuiu.NovikovAA.Sprint5.Task5.V9.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            // Путь к файлу
-            string filePath = "/app/data/AssesmentData/C#/Sprint5Task5/InPutDataFileTask5V9.txt";
+            double result = double.MinValue;
 
-            // Переменная для хранения максимального значения
-            double maxValue = double.MinValue;
-
-            using (StreamReader reader = new StreamReader(filePath))
+            using (StreamReader reader = new StreamReader(path))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
                     if (!string.IsNullOrWhiteSpace(line))
                     {
-                        // Разбиваем строку на отдельные элементы
-                        var values = line.Split(' ');
+                        // Разделяем строку на отдельные элементы
+                        var values = line.Split();
 
                         foreach (var valueStr in values)
                         {
-                            // Преобразуем строку в число
-                            double value = Math.Round(Convert.ToDouble(valueStr), 3);
+                            // Преобразуем строку в число, используя InvariantCulture
+                            double value = Math.Round(double.Parse(valueStr, CultureInfo.InvariantCulture), 3);
 
                             // Проверяем, является ли текущее значение максимальным
-                            if (value > maxValue)
+                            if (value > result)
                             {
-                                maxValue = value;
+                                result = value;
                             }
                         }
                     }
                 }
             }
-            return maxValue;
-            // Выводим найденное максимальное значение
-            Console.WriteLine($"Максимальное значение: {maxValue}");
+
+            return result;
         }
     }
 }
