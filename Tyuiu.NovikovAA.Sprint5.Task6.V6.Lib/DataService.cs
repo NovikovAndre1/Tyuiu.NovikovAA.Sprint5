25d30088724ea2e81c41f7faabcabb2bf9a45bad
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-12-12 04:26:20 +0600

    Task6

---
+++ Tyuiu.NovikovAA.Sprint5.Task6.V6.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Tyuiu.NovikovAA.Sprint5.Task6.V6.Lib
+{
+    public class DataService : ISprint5Task6V6
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int count = 0;
+
+            foreach (char c in path)
+            {
+                if ('а' <= c && c <= 'я') // Проверка, является ли символ строчной русской буквой
+                {
+                    count++;
+                }
+            }
+
+            return count;
+
+        }
+    }
+}
---
+++ Tyuiu.NovikovAA.Sprint5.Task6.V6/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using Tyuiu.NovikovAA.Sprint5.Task6.V6.Lib;
+
+namespace Tyuiu.NovikovAA.Sprint5.Task6.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("********************************************************************");
+            Console.WriteLine("Исходные данные");
+            Console.WriteLine("********************************************************************");
+
+
+            string path = Path.Combine(Path.GetTempPath(), "InPutDataFileTask6V6.txt");
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("********************************************************************");
+            Console.WriteLine("РЕЗУЛЬТАТ");
+            Console.WriteLine("********************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
